f7f5bc261c3b97dc0c46f5488e92c6233bfa79d9
Erekle Khomasuridze <93726684+S4syka@users.noreply.github.com>
2025-02-02 22:28:41 +0400

    Added feedback management

---
+++ Contracts/Repositories/IFeedbackRepository.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Repositories;
+
+public interface IFeedbackRepository
+{
+    public Task<List<Feedback>> GetAllFeedbacksAsync();
+    public Task<Feedback> GetFeedbackByIdAsync(Guid id);
+    public Task<Feedback> CreateFeedbackAsync(Feedback feedback);
+    public Task DeleteQTITestAsync(Feedback qtiTest);
+}
---
+++ Entities/Models/Feedback.cs
@@ -0,0 +1,27 @@
+﻿using Redis.OM.Modeling;
+using Redis.OM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models;
+
+[Document(StorageType = StorageType.Json, Prefixes = new[] { "Feedback" }, IndexName = "Feedbacks", IdGenerationStrategyName = nameof(Uuid4IdGenerationStrategy))]
+public class Feedback
+{
+    [RedisIdField]
+    [Indexed]
+    public Guid Id { get; set; }
+    
+    [Searchable]
+    public string? Email { get; set; }
+    
+    [Searchable]
+    [Required]
+    public string Message { get; set; }
+    
+    public DateTime? Uploaded { get; set; }
+}
